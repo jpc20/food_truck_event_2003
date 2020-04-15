60d65dc9be8148334e0b12d165d9c7ae1965088d
Jack Cullen <jpcullen20@gmail.com>
2020-04-15 10:14:48 -0600

    Add test for food_truck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -7,6 +7,8 @@ class FoodTruckTest < Minitest::Test
 
   def setup
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   def test_it_has_a_name
@@ -15,5 +17,21 @@ class FoodTruckTest < Minitest::Test
 
   def test_inventory_empty_hash_default
     assert_equal ({}), @food_truck.inventory
+  end
+
+  def test_check_stock_0_default
+    assert_equal 0, @food_truck.check_stock(@item1)
+  end
+
+  def test_stock
+    @food_truck.stock(@item1, 30)
+    assert_equal ({@item1 => 30}), @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+
+    @food_truck.stock(@item2, 12)
+    assert_equal ({@item1 => 55, @item2 =>12}), @food_truck.inventory
   end
 end
